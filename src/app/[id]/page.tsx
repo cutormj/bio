b4807441bc3818132b52c166e682750870aed950
Joseph Cutor <cutormarkjoseph@gmail.com>
2024-09-10 18:07:25 +0800

    home updated

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { userAgent } from "next/server";
 import { getUserByUsername, getUsers } from "@/actions/user-action"
+import HomeHero from "@/components/(logged-in)/HomeHero";
 
 import React from 'react'
 
@@ -8,7 +9,11 @@ async function page({params}: {params: {id: string}}) {
   const user = await getUserByUsername(params.id); 
   console.log("HERE IS THE USER!!",user);
   return (
-    <div>{user.email}</div>
+    <div className="theme-maink">
+      <div className="bg-bgPrimary">
+        <HomeHero />
+      </div>
+    </div>
   )
 }
 
